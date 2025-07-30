53af927c8e46bd3bf6c9355b41b74e93edf9be73
namimoon <namimoon@gmail.com>
2025-07-30 22:00:46 +0900

    websocket 수정 중

--- src/main.js
+++ src/main.js
@@ -6,8 +6,18 @@ import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
+// Element Plus Icons 추가
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+
 // 앱 생성 및 등록
 const app = createApp(App)
+
+// 모든 Element Plus 아이콘 컴포넌트 등록
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+
 app.use(router)
 app.use(ElementPlus) // ✅ Element Plus 등록
 app.mount('#app')
