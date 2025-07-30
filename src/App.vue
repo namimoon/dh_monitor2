eb0fb542fdf5fdbfa68af8081697148014cac0ef
namimoon <namimoon@gmail.com>
2025-07-30 22:18:46 +0900

    websocket 수정 중

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <div style="text-align: center; padding: 10px; background-color: #eee;">
-<!--	    <button @click="$router.push('/page1')">1페이지</button>-->
-<!--	    <button @click="$router.push('/page2')">2페이지</button>-->
-<!--	    <button @click="$router.push('/page3')">3페이지</button>-->
+	    <button @click="$router.push('/page1')">1페이지</button>
+	    <button @click="$router.push('/page2')">2페이지</button>
+	    <button @click="$router.push('/page3')">3페이지</button>
 	    <button @click="$router.push('/page4')">4페이지</button>
 	    <button @click="$router.push('/page5')">5페이지</button>
 		<button @click="$router.push('/page6')">6페이지</button>
